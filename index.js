f80cc9d9213c9430f7f8cdc0ce37f856e8ef9e2f
Niv Yulevitch <niv.yulevitch@gmail.com>
2020-08-05 14:26:06 +0300

    do some changes

--- index.js
+++ index.js
@@ -1,120 +1,90 @@
+const genderButton = $('#genderBTN');
+const countriesButton = $('#countriesBTN');
+const table = $('table');
+const tableHeaders = $('#tableHeaders');
+const tableBody = $('#tableBody');
+
 const genderTableHeaders = [
-  { value: "gender", label: "Gender" },
-  { value: "amount", label: "Amount" },
+  { value: "male", label: "Male" },
+  { value: "female", label: "Female" },
 ];
 
-const CountriesTableHeaders = [
+const countryTableHeaders = [
   { value: "country", label: "Country" },
   { value: "amount", label: "Amount" },
 ];
 
-const gTableHeaders = $("#genderTableHeaders");
-const gTableBody = $("#genderTableBody");
-gTableHeaders.append(getRowHeaderItem(genderTableHeaders));
-
-$(async function init() {
+async function init() {
   try {
     const response = await getUsersFromServer({
       url: `https://randomuser.me/api/?results=50`,
     });
     const { results } = response;
-    getRowItem(results);
-    // console.log(getRowItem(results));
+    return results;
   } catch (err) {
-    console.log(err);
     alert(`message: ${err.statusText} , status: ${err.status}`);
   }
-});
+};
 
-function getRowHeaderItem(myHeaders) {
-  const ths = myHeaders
-    .filter((header) => {
-      return header;
-    })
-    .map((header) => {
-      const { label } = header;
-      return _getTH(label);
-    });
+genderButton.on('click', async () => {
+  clearTable()
+  const results = await init();
+  console.log(results);
+  const malesAmount = [];
+  const femalesAmouunt = [];
+  const genders = results.reduce((genderAmountArray, user) => {
+    const { gender } = user;
+    if (gender === "male") {
+      malesAmount.push(gender);
+    } else {
+      femalesAmouunt.push(gender);
+    }
+  }, []);
 
-  const tr = _getTR();
-  tr.append(...ths);
-  return tr;
+  tableHeaders.append(getRowHeaderItem(genderTableHeaders));
+  // tableBody.append(getRowItem())
+  console.log(malesAmount);
+  console.log(femalesAmouunt);
+})
 
-  function _getTR() {
-    return document.createElement("TR");
-  }
+countriesButton.on('click', async () => {
+  clearTable()
+  const results = await init();
+  const countries = results.reduce((countriesObject, user) => {
+    const { country } = user.location;
+    const countriesAmount = countriesObject[country] || 0;
+    return {...countriesObject, [country]:countriesAmount + 1};
+  }, {});
+  console.log(countries);
 
-  function _getTH(value) {
-    const th = document.createElement("TH");
-    th.innerText = value;
-    return th;
-  }
+  tableHeaders.append(getRowHeaderItem(countryTableHeaders));
+  // tableBody.append(getRowItem())
+})
+
+function clearTable(){
+  $("#tableHeaders").html("");
+  $("#tableBody").html("");
 }
 
-function getRowItem(userData) {
-  let femaleAmount = 0;
-  let maleAmount = 0;
+function getRowHeaderItem (myHeaders) {
+  const ths = myHeaders.filter((header) => { return header }).map(header => {
+      const { label } = header
+      return _getTH(label)
+  })
 
-  const users = userData.filter((user) => {
-    return user.gender;
-  });
-  const tds = users.map((user) => {
-    const { gender } = user;
-    return gender;
-  });
-
-  for (let index = 0; index < tds.length; index++) {
-    if (tds[index] == "male") {
-      maleAmount++
-    }
-    else {
-      femaleAmount++
-    }
-  }
-
-  const tr = _getTR();
+  const tr = _getTR()
+  tr.append(...ths)
   return tr
 
-  function _getTR() {
-    return document.createElement("TR");
+  function _getTR () {
+      return document.createElement("TR")
   }
 
-  function _getTD(value) {
-    const td = document.createElement("TD");
-    td.innerText = value;
-    return td;
+  function _getTH (value) {
+      const th = document.createElement("TH")
+      th.setAttribute('scope', 'col');
+      th.innerText = value;
+      return th
   }
-
-  // const { gender } = userData // destructuring es6
-  // // return getRowItem;
-
-  // const tr = _getTR()
-  // const firstRowFromHeaders = headers[ 0 ]
-  // const visibleHeaders = firstRowFromHeaders.filter((header) => { return header.isVisible })
-  // const tds = visibleHeaders.map((header) => {
-  //     const { value } = header
-  //     const currentValue = carData[ value ]
-  //     return _getTD(currentValue)
-  // })
-
-  // // const tdLP = _getTD(lp);
-  // // const tdColor = _getTD(color);
-  // // const tdType = _getTD(type);
-  // // const tdDoors = _getTD(doors);
-  // tr.append(...tds)
-  // return tr
-  // function _getTR () {
-  //     return document.createElement("TR")
-  // }
-
-  // function _getTD (value) {
-  //     const allowedTypes = [ "string", "number", "boolean" ]
-  //     const theType = typeof value
-  //     // if (!allowedTypes.includes(theType)) return;
-  //     let currentValue = !allowedTypes.includes(theType) ? "-" : value
-  //     // if (theType !== "string" && theType !== "number") return;
-  //     const td = document.createElement("TD")
-  //     td.innerText = currentValue
-  //     return td
-  // }
 }
+
